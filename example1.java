6f130f2a70cc406215bf98329f541f93c588556f
HumpBackJack <147660746+HumpBackJack@users.noreply.github.com>
2025-10-29 00:37:07 -0400

    gwr

--- example1.java
+++ example1.java
@@ -1,60 +1,31 @@
-//Jack G
-//10/27/25
-public class example1{
-    public static void main(String[] args) {
-    countFactors(24);
-    isPrime(3);
-    countPrimes(3);
+public class example1 {
+    public static void main(String[]args){
+        System.out.println(countFactors(5));
+        System.out.println(isPrime(4));
+        System.out.println(countPrimes(1000));
     }
-        public static int countFactors (int num){
+    public static int countFactors(int num){
         int count = 0;
-        int answer = -1;
-        while(count!=num){
-        count++;
-        answer = num%count;
-        if(answer == 0){
-            System.out.print(count +",");
+        int i = 1;
+        while(i<=num){
+            if (num%i == 0)
+            count++;
+            i++;
         }
-        }
-        return answer;
-    }
-       public static boolean isPrime(int num2){
-        int count = 1;
-        boolean result = true;
-        boolean end = false;
-        while(count<num2-1&& end == false){
-          count ++;
-          if (num2%count == 0){
-            result = false;
-            System.out.println("it is not a prime number");
-            end = true;
-          }
-        }
-        System.out.println("it is a prime number");
-        return result;
-       }
-       public static boolean isPrimei(int num2){
-        int count = 1;
-        boolean result = true;
-        boolean end = false;
-        while(count<num2-1&& end == false){
-          count ++;
-          if (num2%count == 0){
-            result = false;
-            end = true;
-          }
-        }
-        return result;
-       }
-           public static int countPrimes(int n) {
-        int count = 0;
-
-        for (int i = 2; i <= n; i++) {
-            if (isPrimei(i)) {
-                count++;
-            }
-        }
-        System.out.println(count);
         return count;
+    }
+    public static boolean isPrime(int j){
+        return (countFactors(j) == 2);
+    }
+    public static int countPrimes(int u){
+        int count2 = 0;
+        int o = 2;
+        while(o<u){
+            if(isPrime(o)){
+                count2++;
+            }
+            o++;
+        }
+        return count2;
     }
 }
--- example3.java
+++ example3.java
@@ -1,40 +1,38 @@
 // Jack G
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class example3 {
     public static void main(String[] args) {
-        Random rand = new Random();
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        int target = rand.nextInt(101); 
+        int target = (int)(Math.random()*101); 
 
-        System.out.println("I'm thinking of a number between 0 and 100.");
+        System.out.println("I'm thinking of a number between 0 and 100");
         System.out.print("Make your first guess: ");
 
-        int guess = scanner.nextInt();           
-        int guessesCount = 1;                    
+        int guess = scan.nextInt();           
+        int Count = 1;                    
         while (guess != target) {
             if (guess < target) {
                 System.out.println("Too low!");
-            } else { // guess > target
+            } else {
                 System.out.println("Too high!");
             }
 
             System.out.print("Try again: ");
-            guess = scanner.nextInt();          
-            guessesCount++;                     
+            guess = scan.nextInt();          
+            Count++;                     
         }
 
         System.out.println("You got it! The number was " + target + ".");
-        System.out.println("You took " + guessesCount + (guessesCount == 1 ? " guess." : " guesses."));
-        if (guessesCount <= 7) {
-            System.out.println("Nice! You found it in " + guessesCount);
+        System.out.println("You took " + Count + "tries");
+        if (Count <= 7) {
+            System.out.println("Nice! You found it in " + Count);
         } else {
             System.out.println("You took more than 7 guesses");
         }
 
-        scanner.close();
+        scan.close();
     }
 }
